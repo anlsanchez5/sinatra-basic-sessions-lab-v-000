890c924d55b3083cccaf71520ed6ac3ae22debbe
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-03-28 02:35:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,11 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
   get '/' do
     erb :index
   end
 
   post '/checkout' do
-  
+
   end
 end
